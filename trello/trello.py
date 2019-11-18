28f6adc23bfad511f2aa3f32b33ff3ffd5233b88
Dan Sepich <dan.sepich@gmail.com>
2019-11-18 16:33:35 -0600

    No longer creates new board 'each day'. Archives 'Done' items each day.

--- trello/trello.py
+++ trello/trello.py
@@ -10,7 +10,7 @@ propFile = 'trello.yaml'
 
 def create_board():
     url = "https://api.trello.com/1/boards/"
-    querystring = {"name":str(date.today()) + " - Daily To Do's","defaultLabels":"true","defaultLists":"true","keepFromSource":"none","prefs_permissionLevel":"private","prefs_voting":"disabled","prefs_comments":"members","prefs_invitations":"members","prefs_selfJoin":"true","prefs_cardCovers":"true","prefs_background":"blue","prefs_cardAging":"regular","key":key,"token":token}
+    querystring = {"name":"Daily To Do's","defaultLabels":"true","defaultLists":"true","keepFromSource":"none","prefs_permissionLevel":"private","prefs_voting":"disabled","prefs_comments":"members","prefs_invitations":"members","prefs_selfJoin":"true","prefs_cardCovers":"true","prefs_background":"blue","prefs_cardAging":"regular","key":key,"token":token}
 
     response = requests.request("POST", url, params=querystring)
     board_id=response.json()['id']
@@ -20,7 +20,6 @@ def get_lists(board_id, list_name):
     url = "https://api.trello.com/1/boards/"+board_id+"/lists"
     querystring = {"cards":"none","card_fields":"all","filter":"open","fields":"all","key":key,"token":token}
     response = requests.request("GET", url, params=querystring)
-
     for list in response.json():
         if list['name'] == list_name:
             return list['id']
@@ -52,10 +51,34 @@ def getListFromProps(keyName):
             if(key == keyName):
                 return val
 
+def boardExists(boardID):
+    url = "https://api.trello.com/1/boards/"+boardID
+    querystring = {"key":key,"token":token}
+    response = requests.request("GET", url, params=querystring)
+    return(response)
+
+def cleanupList(listID):
+    url = "https://api.trello.com/1/lists/"+listID+"/archiveAllCards"
+    querystring = {"key":key,"token":token}
+    response = requests.request("POST", url, params=querystring)
+    return(response)
+
 def main():
-    board_id = create_board()
-    add_card(get_lists(board_id, 'To Do'))
-    text_link(board_id)
+    boardID = '0'
+    if os.path.exists("activeBoard.txt"):
+        with open("activeBoard.txt") as f: # The with keyword automatically closes the file when you are done
+            boardID = f.read().replace('\n', '')
+    if str(boardExists(boardID)) == '<Response [200]>':
+        print('Board Exists')
+        cleanupList(get_lists(boardID,'Done'))
+    else:
+        print('Board does not exist, creating new board')
+        boardID = create_board()
+        add_card(get_lists(boardID, 'To Do'))
+        with open("activeBoard.txt", 'w') as f:
+            f.write(boardID)
+
+    text_link(boardID)
 
 if __name__ == '__main__':
         main()
