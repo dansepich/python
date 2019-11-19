eaae7e0e4050b150c1fa1c0506feb8a7c977e2d7
Dan Sepich <dan.sepich@gmail.com>
2019-11-19 10:58:06 -0600

    Cleaned up variable names

--- trello/trello.py
+++ trello/trello.py
@@ -6,87 +6,84 @@ import yaml
 
 key = os.environ['trelloKey']
 token = os.environ['trelloToken']
-propFile = 'trello.yaml'
+prop_file = 'trello.yaml'
 
 def create_board():
     url = "https://api.trello.com/1/boards/"
-    querystring = {"name":"Daily To Do's","defaultLabels":"true","defaultLists":"true","keepFromSource":"none","prefs_permissionLevel":"private","prefs_voting":"disabled","prefs_comments":"members","prefs_invitations":"members","prefs_selfJoin":"true","prefs_cardCovers":"true","prefs_background":"blue","prefs_cardAging":"regular","key":key,"token":token}
+    query_string = {"name":"Daily To Do's","defaultLabels":"true","defaultLists":"true","keepFromSource":"none","prefs_permissionLevel":"private","prefs_voting":"disabled","prefs_comments":"members","prefs_invitations":"members","prefs_selfJoin":"true","prefs_cardCovers":"true","prefs_background":"blue","prefs_cardAging":"regular","key":key,"token":token}
 
-    response = requests.request("POST", url, params=querystring)
+    response = requests.request("POST", url, params=query_string)
     board_id=response.json()['id']
     return board_id
 
-def get_lists(board_id, list_name):
+def get_list(board_id, list_name):
     url = "https://api.trello.com/1/boards/"+board_id+"/lists"
-    querystring = {"cards":"none","card_fields":"all","filter":"open","fields":"all","key":key,"token":token}
-    response = requests.request("GET", url, params=querystring)
+    query_string = {"cards":"none","card_fields":"all","filter":"open","fields":"all","key":key,"token":token}
+    response = requests.request("GET", url, params=query_string)
     for list in response.json():
         if list['name'] == list_name:
             return list['id']
 
     return 'No ID Found'
 
-def add_card(list_id):
-    addCardsURL = "https://api.trello.com/1/cards"
-    existingCardsURL = "https://api.trello.com/1/lists/"+list_id+"/cards/"
-    querystring = {"key":key,"token":token}
-    existingCardsDetails = requests.request("GET", existingCardsURL, params=querystring)
-    for todo in getListFromProps('todos'):
-        if not todo in existingCardsDetails.text:
-            querystring = {"idList":list_id,"keepFromSource":"all","key":key,"token":token,"name":todo}
-            response = requests.request("POST", addCardsURL, params=querystring)
+def add_cards(list_id):
+    add_cardss_URL = "https://api.trello.com/1/cards"
+    existing_cards_URL = "https://api.trello.com/1/lists/"+list_id+"/cards/"
+    query_string = {"key":key,"token":token}
+    existing_cards_details = requests.request("GET", existing_cards_URL, params=query_string)
+    for todo in get_list_from_props('todos'):
+        if not todo in existing_cards_details.text:
+            query_string = {"idList":list_id,"keepFromSource":"all","key":key,"token":token,"name":todo}
+            response = requests.request("POST", add_cardss_URL, params=query_string)
 
 
 def text_link(board_id):
     url = "https://api.trello.com/1/boards/"+board_id
 
-    querystring = {"actions":"all","boardStars":"none","cards":"none","card_pluginData":"false","checklists":"none","customFields":"false","fields":"name,desc,descData,closed,idOrganization,pinned,url,shortUrl,prefs,labelNames","lists":"open","members":"none","memberships":"none","membersInvited":"none","membersInvited_fields":"all","pluginData":"false","organization":"false","organization_pluginData":"false","myPrefs":"false","tags":"false","key":key,"token":token}
-    response = requests.request("GET", url, params=querystring)
-    for number in getListFromProps('numbers'):
+    query_string = {"fields":"url","key":key,"token":token}
+    response = requests.request("GET", url, params=query_string)
+    for number in get_list_from_props('numbers'):
         print(str(number)+'@mms.att.net', str(date.today())+' Trello Board: '+ response.json()['url'])
         textAlert.sendEmail(os.environ['gmail'], os.environ['gmailpswd'], str(number)+'@mms.att.net', 'Trello Board', str(date.today())+' Trello Board: '+ response.json()['url'])
 
 
-def getListFromProps(keyName):
-    with open(propFile) as file:
+def get_list_from_props(key_name):
+    with open(prop_file) as file:
         documents = yaml.full_load(file)
         for key, val in documents.items():
-            if(key == keyName):
+            if(key == key_name):
                 return val
 
-def boardExists(boardID):
-    url = "https://api.trello.com/1/boards/"+boardID
-    querystring = {"key":key,"token":token}
-    response = requests.request("GET", url, params=querystring)
+def board_exists(board_ID):
+    url = "https://api.trello.com/1/boards/"+board_ID
+    query_string = {"key":key,"token":token}
+    response = requests.request("GET", url, params=query_string)
     return(response)
 
-def cleanupList(listID):
-    url = "https://api.trello.com/1/lists/"+listID+"/archiveAllCards"
-    querystring = {"key":key,"token":token}
-    response = requests.request("POST", url, params=querystring)
+def cleanup_list(list_ID):
+    url = "https://api.trello.com/1/lists/"+list_ID+"/archiveAllCards"
+    query_string = {"key":key,"token":token}
+    response = requests.request("POST", url, params=query_string)
     return(response)
 
 def main():
-    boardID = '0'
+    board_ID = '0'
     if os.path.exists("activeBoard.txt"):
         with open("activeBoard.txt") as f: # The with keyword automatically closes the file when you are done
-            boardID = f.read().replace('\n', '')
-    if str(boardExists(boardID)) == '<Response [200]>':
-        print('Board Exists')
+            board_ID = f.read().replace('\n', '')
+    if str(board_exists(board_ID)) == '<Response [200]>':
         ##Archive cards in Done column
-        cleanupList(get_lists(boardID,'Done'))
+        cleanup_list(get_list(board_ID,'Done'))
         ##Add default items to list
-        add_card(get_lists(boardID,'To Do'))
-        ##Delete Duplicates in To Do column
+        add_cards(get_list(board_ID,'To Do'))
 
     else:
-        print('Board does not exist, creating new board')
-        boardID = create_board()
-        add_card(get_lists(boardID, 'To Do'))
+        board_ID = create_board()
+        add_cards(get_list(board_ID, 'To Do'))
         with open("activeBoard.txt", 'w') as f:
-            f.write(boardID)
+            f.write(board_ID)
 
-    text_link(boardID)
+    text_link(board_ID)
 
 if __name__ == '__main__':
         main()
